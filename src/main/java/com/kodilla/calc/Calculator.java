24da60a99e7e0d039c3721608e0b14b249bb48c7
Mateusz Dudek <mateuszdudek@MacBook-Air-Mateusz.local>
2020-01-08 20:57:43 +0100

    First Commit

--- src/main/java/com/kodilla/calc/Calculator.java
+++ src/main/java/com/kodilla/calc/Calculator.java
@@ -2,29 +2,21 @@ package com.kodilla.calc;
 
 public class Calculator {
 
-    private double a;
-    private double b;
-
-    public Calculator(double a, double b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public double add (){
+    public double add (double a, double b){
         return a+b;
     }
 
-    public double substract () {
+    public double substract (double a, double b) {
         return a-b;
     }
 
 
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator(45.3, 22.5);
+        Calculator calculator = new Calculator();
 
-        double addResult = calculator.add();
-        double substractResult = calculator.substract();
+        double addResult = calculator.add(35.5, 44.3);
+        double substractResult = calculator.substract(50.2, 44.2);
 
         System.out.println(addResult);
         System.out.println(substractResult);
